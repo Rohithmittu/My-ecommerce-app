ec3d6598cdea3fc7a3ca2d77621b6271831312a9
Rohithmittu <guntukurohith927@gmail.com>
2023-06-25 17:01:37 +0530

    reset password has been completed

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,24 @@ module.exports = (err,req,res,next) =>{
         const message = `Resource not found. Invalid:${err.path}`
         err = new ErrorHandler(message,400);
     }
+
+    // Mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400);
+    }
+
+    // wrong JWT error
+    if(err.name === "jsonwebTokenError"){
+        const message = `json Web tokenis is invalid, try again`
+        err = new ErrorHandler(message,400);
+    }
+
+    // JWT EXPIRE error
+    if(err.name === "jsonwebExpiredError"){
+        const message = `json Web tokenis is invalid, try again`
+        err = new ErrorHandler(message,400);
+    }
     
 
     res.status(err.statusCode).json({

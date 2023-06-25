3eee7c69828518ea7cd13d6a12e579d6229d3620
Rohithmittu <guntukurohith927@gmail.com>
2023-06-25 12:12:35 +0530

    added logout feature, and implimented a feature that aonly admin can access the website to add,delete,upadate a product details,no other user will be able to acess the data other than admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,17 +4,20 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
-  getProductDetails
-  
+  getProductDetails,
 } = require("../controllers/productController");
+const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/products/new").post(createProduct);
+router.route("/products/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
-router.route("/products/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
-
+router
+  .route("/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+  .get(getProductDetails);
 
 module.exports = router;
